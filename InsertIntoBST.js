7b8f5ce17a98d124ac6a7c4ad63a1fba05928d59
Jerard Dayanghirang Guevarra <jerardlguevarra@gmail.com>
2020-06-15 18:57:14 -0400

    Create InsertIntoBST.js

---
+++ InsertIntoBST.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+
+var insertIntoBST = function(root, val) {
+   if(!root) return new TreeNode(val)
+   if(root.val < val){
+       if(!root.right){
+           let node = new TreeNode(val)
+           root.right = node
+           return root
+       }
+       insertIntoBST(root.right,val)
+   }else if(root.val > val){
+       if(!root.left){
+           let node = new TreeNode(val)
+           root.left = node
+           return root
+       }
+       insertIntoBST(root.left,val)
+   } 
+   return root 
+};
